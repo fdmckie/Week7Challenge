c5565496b7d5b303ae08631b1526952c7df9dd33
fdmckie <51306208+fdmckie@users.noreply.github.com>
2019-10-17 16:24:15 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBootSecurityTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityTemplateApplication.class, args);
+    }
+
+}
