c07df900d05f393a4df49a6d46aa5fcf641bea78
fdmckie <fdmckie@gmail.com>
2019-10-21 13:38:47 -0400

    Week 7 Challenge Update

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -48,7 +50,17 @@ public class UserService {
         userRepository.save(user);
     }
 
-
+    // returns currently logged in user
+//    public User getUser(){
+//
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//        String currentusername = authentication.getName();
+//
+//        User user = userRepository.findByUsername(currentusername);
+//
+//        return user;
+//    }
 
 }
 
